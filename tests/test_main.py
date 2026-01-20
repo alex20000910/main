e3ef05d166d0063f3ac6530fa5f042c548c9e133
Chih-Keng Hung <alex1010512@gmail.com>
2026-01-20 13:28:32 +0800

    test
    Signed-off-by: Chih-Keng Hung <alex1010512@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,74 +21,6 @@ from typing import override, Literal
     
 VERSION = sys.version.split()[0]
 VERSION = int(''.join(VERSION.split('.')))
-if VERSION < 3130:
-    REQUIREMENTS = ["numpy==1.26.4",
-    "opencv-python==4.10.0.84",
-    "matplotlib==3.10.5",
-    "xarray==2025.7.1",
-    "h5py==3.14.0",
-    "Pillow==11.3.0",
-    "scipy==1.16.1",
-    "lmfit==1.3.4",
-    "tqdm==4.67.1",
-    "pywin32==311",
-    "originpro==1.1.13",
-    "py-cpuinfo==9.0.0",
-    "psutil==7.0.0",
-    "zarr==3.1.1",
-    "PyQt5==5.15.11",
-    "pyqtgraph==0.13.7",
-    "tkinterdnd2==0.4.3"
-    ]
-else:
-    REQUIREMENTS = ["numpy==2.2.6",
-    "opencv-python==4.12.0.88",
-    "matplotlib==3.10.5",
-    "xarray==2025.7.1",
-    "h5py==3.14.0",
-    "Pillow==11.3.0",
-    "scipy==1.16.1",
-    "lmfit==1.3.4",
-    "tqdm==4.67.1",
-    "pywin32==311",
-    "originpro==1.1.13",
-    "py-cpuinfo==9.0.0",
-    "psutil==7.0.0",
-    "zarr==3.1.1",
-    "PyQt5==5.15.11",
-    "pyqtgraph==0.13.7",
-    "tkinterdnd2==0.4.3",
-    "google-crc32c==1.8.0"  # for numcodecs
-    ]
-
-def restart():
-    if os.name == 'nt':
-        os.system('python -W ignore::SyntaxWarning -W ignore::UserWarning "'+os.path.abspath(inspect.getfile(inspect.currentframe()))+'"')
-    elif os.name == 'posix':
-        try:
-            os.system('python3 -W ignore::SyntaxWarning -W ignore::UserWarning "'+os.path.abspath(inspect.getfile(inspect.currentframe()))+'"')
-        except:
-            os.system('python -W ignore::SyntaxWarning -W ignore::UserWarning "'+os.path.abspath(inspect.getfile(inspect.currentframe()))+'"')
-
-def install(s: str = ''):
-    print('Some Modules Not Found')
-    # a = input('pip install all the missing modules ???\nProceed (Y/n)? ')
-    a = 'y'
-    if a.lower() == 'y':
-        if s == '':
-            try:
-                for i in REQUIREMENTS:
-                    subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", i])
-            except subprocess.CalledProcessError:
-                for i in REQUIREMENTS:
-                    subprocess.check_call([sys.executable, "-m", "pip3", "install", "--user", i])
-        else:
-            try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", s])
-            except subprocess.CalledProcessError:
-                subprocess.check_call([sys.executable, "-m", "pip3", "install", "--user", s])
-    else:
-        quit()
 
 try:
     import matplotlib
@@ -127,8 +59,6 @@ try:
         import pyqtgraph
         from tkinterdnd2 import DND_FILES, TkinterDnD
 except ModuleNotFoundError:
-    install()
-    restart()
     quit()
 
 try:
