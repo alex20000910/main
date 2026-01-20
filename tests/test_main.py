46860b5a9dffed2693da876f16c4a3ab2bb22edd
Chih-Keng Hung <alex1010512@gmail.com>
2026-01-20 17:04:04 +0800

    test
    Signed-off-by: Chih-Keng Hung <alex1010512@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -78,5 +78,5 @@ except ImportError as e:
     print(e)
     
 path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'test_data', 'simulated_R1_5.0_R2_0.h5'))
-lfs = loadfiles(path)
+lfs = loadfiles([path], mode ='eager')
 assert isinstance(lfs, FileSequence)
