4d80815589c8744c6f5ca15ff7b692a7daad459c
Chih-Keng Hung <alex1010512@gmail.com>
2026-01-29 17:54:42 +0800

    update test
    Signed-off-by: Chih-Keng Hung <alex1010512@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -567,6 +567,8 @@ def test_mfit_data():
     ml = mloader(st, data, ev, phi, rdd, cdir, lowlim)
     ml.loadparam('0', '0', True, 1)
     ml.loadmfit_(os.path.join(cdir, 'tests', 'rev_PE10A20f -170 VIV_pass.vms'))
+    ml.loadmfit_re(os.path.join(cdir, 'tests', 'PE10A20f -170 VIV.vms'))
+    ml.loadmfit_2p(os.path.join(cdir, 'tests', 'rev_PE10A20f -170 VIV.vms'))
     ml.loadmfit_(os.path.join(cdir, 'tests', 'simulated_R1_14.0_R2_0_mfit.npz'))
     mdata = mfit_data(cdir=cdir)
     ko, fev, rpos, ophi, fwhm, mpos, kmin, kmax, skmin, skmax, smaa1, smaa2, smfp, smfi, smresult, smcst, fpr, mdet = mdata.get()
